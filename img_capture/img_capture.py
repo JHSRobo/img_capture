e2ab5c946dbc5d1b3667ea120c8329590e68218c
jack-frings <jackdfrings@protonmail.com>
2024-11-30 17:39:30 -0800

    program to record camera feed from topside to ops

---
+++ img_capture/img_capture.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node 
+import cv2
+from cv_bridge import CvBridge 
+from sensor_msgs.msg import Image 
+import os
+
+class ImageCaptureNode(Node):
+    def __init__(self):
+        super().__init__("img_capture")
+        self.log = self.get_logger()
+        self.log.info("Image Capture Node Online")
+
+        # Global Variables 
+        self.path = "/home/jhsrobo/corews/src/img_capture/img"
+        self.count = 1
+        self.cropped_count = 1
+        self.bridge = CvBridge()
+        self.cropping = False
+
+        # cropping Parameter
+        self.declare_parameter("cropping", self.cropping)
+        self.create_timer(0.1, self.update_parameters)
+        
+        self.camera_feed_subscriber = self.create_subscription(Image, "camera_feed", self.img_callback, 10)
+
+        def update_parameters(self):
+            self.cropping = self.get_parameter("cropping").value
+
+        def img_callback(self, screenshot):
+            img = self.bridge.imgmsg_to_cv2(screenshot, desired_encoding="passthrough")
+            if self.cropping:
+                img = img[30:720, 0:880]
+                cv2.imwrite(f"{self.path}/cropped/{self.cropped_count}.png", img)
+                self.cropped_count += 1 
+            else:
+                cv2.imwrite(f"{self.path}/full/{self.count}.png", img)
+                self.count += 1 
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ImageCaptureNode()
+
+    # Delete prior images
+    os.system("rm -rf /home/jhsrobo/corews/src/img_capture/cropped/*.png")
+    os.system("rm -rf /home/jhsrobo/corews/src/img_capture/full/*.png")
+
+    rclpy.spin(node)
+
+    cv2.destroyAllWindows()
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
