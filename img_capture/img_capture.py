a821ecb6b4285c65b8183eff587dbb4aaa09a3b2
jack-frings <jackdfrings@protonmail.com>
2024-11-30 18:38:23 -0800

    fixed indentation issue

--- img_capture/img_capture.py
+++ img_capture/img_capture.py
@@ -20,24 +20,28 @@ class ImageCaptureNode(Node):
         self.bridge = CvBridge()
         self.cropping = False
 
-        # cropping Parameter
-        self.declare_parameter("cropping", self.cropping)
+        # Cropping Parameter
         self.create_timer(0.1, self.update_parameters)
+        self.declare_parameter("cropping", self.cropping)
         
         self.camera_feed_subscriber = self.create_subscription(Image, "camera_feed", self.img_callback, 10)
 
-        def update_parameters(self):
-            self.cropping = self.get_parameter("cropping").value
+    def update_parameters(self):
+        change = (self.cropping != self.get_parameter("cropping").value)
+        self.cropping = self.get_parameter("cropping").value
 
-        def img_callback(self, screenshot):
-            img = self.bridge.imgmsg_to_cv2(screenshot, desired_encoding="passthrough")
-            if self.cropping:
-                img = img[42:720, 0:1150]
-                cv2.imwrite(f"{self.path}/cropped/{self.cropped_count}.png", img)
-                self.cropped_count += 1 
-            else:
-                cv2.imwrite(f"{self.path}/full/{self.count}.png", img)
-                self.count += 1 
+        if change:
+            self.log.info(f"Cropping: {self.cropping}")
+
+    def img_callback(self, screenshot):
+        img = self.bridge.imgmsg_to_cv2(screenshot, desired_encoding="passthrough")
+        if self.cropping:
+            img = img[42:720, 0:1150]
+            cv2.imwrite(f"{self.path}/cropped/{self.cropped_count}.png", img)
+            self.cropped_count += 1 
+        else:
+            cv2.imwrite(f"{self.path}/full/{self.count}.png", img)
+            self.count += 1 
 
 def main(args=None):
     rclpy.init(args=args)
